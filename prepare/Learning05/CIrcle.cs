24461f1e345fd1a0421d983c9612ba2ce04188b3
rchristian24 <chr22055@byui.edu>
2023-02-27 15:34:41 -0700

    Shapes

---
+++ prepare/Learning05/CIrcle.cs
@@ -0,0 +1,13 @@
+using System;
+public class Circle : Shape
+{
+    private double _radius;
+    public Circle(String color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square shape1 = new Square("blue", 2);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("green", 3, 2);
+        shapes.Add(shape2);
+
+        Circle shapes3 = new Circle("red", 3);
+        shapes.Add(shapes3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+    }
+    public static void DisplayShape(Shape shape)
+    {
+
     }
 }
